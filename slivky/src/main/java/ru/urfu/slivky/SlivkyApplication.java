a7ba657927636bef3ae81bdfc987cf5c9ddb3689
iluvatarrr <kds02122004@mail.ru>
2026-03-30 21:24:45 +0500

    feat: добавил модуль бэкенда

---
+++ slivky/src/main/java/ru/urfu/slivky/SlivkyApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.slivky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlivkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlivkyApplication.class, args);
+    }
+
+}
---
+++ slivky/src/test/java/ru/urfu/slivky/SlivkyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.slivky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlivkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
